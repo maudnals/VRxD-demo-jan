87f730ade1005f80fe66d70a9d3c03ec3bc36e0e
Maud Nalpas <maud.nalpas@gmail.com>
2017-01-22 23:16:43 +0100

    Video: last try to have it play on mobile

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
-let groups = ['group1', 'group2', 'group4']
+// document.addEventListener("DOMContentLoaded", function(event) {
 
-groups.forEach(function(g) {
-
-    let groupId = '#' + g;
-    let thumb = document.querySelector(`${groupId} a-image`);
-    let video = document.querySelector(`${groupId} a-video`);
-
-    if (thumb) {
-
-        thumb.addEventListener('click', function(e) {
-            video.setAttribute('visible', 'true');
-            video.emit('show');
-            video.play();
+    document.getElementById('go-button').addEventListener('click', function(e) {
+        //video.play();
+        console.log('click');
+        let videos = document.querySelectorAll(`a-video`);
+        videos.forEach(function(v){
+            v.play();
         });
-
-        thumb.addEventListener('mouseleave', function(e) {
-            video.emit('hide');
-            video.setAttribute('visible', 'false');
-            video.pause();
-        });
-    }
-
-    document.querySelector('.a-enter-vr-button').addEventListener('click', function(e){
-        video.play();
     });
-});
 
+    let groups = ['group1', 'group2', 'group4']
 
+    groups.forEach(function(g) {
+
+        let groupId = '#' + g;
+        let thumb = document.querySelector(`${groupId} a-image`);
+        let video = document.querySelector(`${groupId} a-video`);
+
+        if (thumb) {
+
+            thumb.addEventListener('click', function(e) {
+                video.setAttribute('visible', 'true');
+                video.emit('show');
+                video.play();
+            });
+
+            thumb.addEventListener('mouseleave', function(e) {
+                video.emit('hide');
+                video.setAttribute('visible', 'false');
+                video.pause();
+            });
+        }
+    });
+
+// });
